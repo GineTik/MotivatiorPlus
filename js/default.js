12cfa7bb92fa22a3c19fd0cd65920043534b55d5
Artur358 <itgamevenpc@gmail.com>
2022-01-14 12:22:34 +0200

    add menu on phone on all page

--- js/default.js
+++ js/default.js
@@ -8,12 +8,12 @@ window.onload = function() {
         var close_icon = document.querySelector("ion-icon[name*='close']");
 
         if (classIsFind(left_header, "active")) {
-            header_content.style.display = "flex";
+            header_content.style.visibility = "visible";
             menu_icon.style.display = "none";
             close_icon.style.display = "block";
         }
         else {
-            header_content.style.display = "none";
+            header_content.style.visibility = "hidden";
             menu_icon.style.display = "block";
             close_icon.style.display = "none";
         }
@@ -23,4 +23,14 @@ window.onload = function() {
 
 function classIsFind(elem, className) {
     return elem.classList.contains(className);
+}
+
+window.onresize = function() {
+    var header_content = document.querySelector(".header-content");
+    var left_header = document.querySelector(".left-header");
+    if (document.body.clientWidth > 744) {
+        header_content.style.visibility = "visible";
+    } else if (!classIsFind(left_header, "active")) {
+        header_content.style.visibility = "hidden";
+    }
 }
